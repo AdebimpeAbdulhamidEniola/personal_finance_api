732dfdebdd54d63e14650be980e5adca1e8ed32a
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2026-03-08 14:49:39 +0100

    update verify route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -9,6 +9,6 @@ const routes = Router({caseSensitive: true, strict: true});
 routes.post('/signup', validateBody(signUpSchema), signUp);
 routes.post('/login', validateBody(logInSchema), logIn);
 routes.post('/google', googleAuth);
-routes.get('/verify-email/:token', verifyEmail);
+routes.get('/verify-email', verifyEmail);
 
 export default routes;
